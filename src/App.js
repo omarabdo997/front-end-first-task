0151e3fbfec37c4cdc57f64d43176ccfbda9dfcc
omar <you@example.com>
2018-10-27 00:18:50 +0200

    front end first task

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+class App extends Component{
+    state ={log:true,color:'blue',colorshow:'green'}
+    
+    
+    
+    changeState=() => {
+        this.setState((state) =>({
+            log:!state.log
+        })
+        )}
+        changeState2=() => {
+            if(this.state.color=='blue')
+            {
+                this.setState((state) => ({
+                    color:state.color='green',
+                    colorshow:state.colorshow='grey'
+                }))
+            }
+            if(this.state.color=='green')
+            {
+                this.setState((state) => ({
+                    color:state.color='grey',
+                    colorshow:state.colorshow='blue'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+                }))
+            }
+            if(this.state.color=='grey')
+            {
+                this.setState((state) => ({
+                    color:state.color='blue',
+                    colorshow:state.colorshow='green'
+
+                }))
+            }
+            
+        }
+
+    render(){
+        console.log(this.state.color)
+        return <div>
+            <button className='buttonContainer' onClick={this.changeState}>A</button>
+            
+            <p className='text'>{this.state.colorshow}</p>
+            <button className='buttonContainer left' onClick={this.changeState2}>B</button>
+            {this.state.log?<p className={'container'+" "+ this.state.color} >sajdkhaksjdhkjashdkjashkdjsahkdhkasjkdhkjashdkjsahdjhkasdkasdsad
+        askdjkalsjdklasjdlkjaskldjklasjdlkasjkldjlsadjklasjkdklsad
+        dkasjdklasjdlksajdlkajslkdjklsajdlkasjdklsadakdsjhsakjdhkjashdks
+        asdkasdkjaskljdlkasjdklasjkldjklsajdklasjdklsad
+        asdlkajslkdjaskldjklsajdklsajdlkasdj
+    </p>:null}
+            
+        </div>
+    }
+        
+} 
 
 export default App;
